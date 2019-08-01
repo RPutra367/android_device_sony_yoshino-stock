108eb09b7e546a419b87b3997ee1a8793e2cbb3e
sjllls <2012ji123@gmail.com>
2019-08-01 08:37:00 +0800

    common.mk: Add missing USB Hal

--- common.mk
+++ common.mk
@@ -406,6 +406,8 @@ PRODUCT_COPY_FILES += \
 
 # USB
 PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0 \
+    android.hardware.usb@1.1 \
     android.hardware.usb@1.0-service \
     com.android.future.usb.accessory
 
